d04eff1f5816e04d305ed47fc6671aa75c7502c8
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2024-08-20 22:23:01 +0530

    push

--- server/index.js
+++ server/index.js
@@ -15,14 +15,24 @@ dotenv.config();
 //
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
-app.use(cors({
-    origin: 'https://mern-project-frontend-seven.vercel.app',
-    credentials: true,
-    methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],
-    optionsSuccessStatus: 200,
+const whitelist = [
+  '*'
+];
 
-  }));
+app.use((req, res, next) => {
+  const origin = req.get('referer');
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === 'OPTIONS') res.sendStatus(200);
+  else next();
+});
+
 
 app.use(function (request, response, next) {
     response.header("Access-Control-Allow-Origin", "*");
